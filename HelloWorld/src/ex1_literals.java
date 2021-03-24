b765cff79d684e5a85acf6cf64cd1c8d8d863056
pydevnepal <rajukhatri054@gmail.com>
2021-03-24 09:18:01 +0545

    Add literal exercise

--- HelloWorld/src/ex1_literals.java
+++ HelloWorld/src/ex1_literals.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ex1_literals {
+
+/*
+                     Literals
+   It is the constant value which is assigned to the variable. Below are the individual comments for the respective
+   lines of code.
+
+   12. Here 50 is the integer literal
+   13. Naming convention for the character literal in java is to use single('') quote not double.
+   14. By default precision numbers are considered as double by java compiler so we have to use f at the end of the
+       number
+   19. For long integer similar is the case. We have to use (l/L) to denote long since by default the number is integer
+
+*/
+public class ex1_literals {
+    public static void main(String[] args) {
+        byte age = 50;
+        char ch = 'l';
+        float f = 3.6f;
+        double d = 3.6;
+        long ln = 5555555555555555555l;
+        String st = "Amit";
+        System.out.println(age);
+    }
 }
