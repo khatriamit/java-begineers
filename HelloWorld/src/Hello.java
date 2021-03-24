961e850afc86b7ebc9d800f07b775fe6b9af3f34
pydevnepal <rajukhatri054@gmail.com>
2021-03-24 09:17:15 +0545

    Add docs int the code

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,9 +1,13 @@
 
-// My first Java Program
+/*                          Naming conventions in Java
+    1.For class name we use PascalCasing naming convention in Java (class TestClass)
+    2.For function name we use camelCasing naming convention in Java (public static void testFunction)
+    3. Double forward slash '//' are used for single line comments
+*/
 
 
 public class Hello {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello, Raju");
     }
 }
