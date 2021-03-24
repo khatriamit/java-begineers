35c6193d5f5d1773ca0e09cec7309d10d269554d
pydevnepal <rajukhatri054@gmail.com>
2021-03-24 09:18:25 +0545

    Add input output exercise

--- HelloWorld/src/ex2_io.java
+++ HelloWorld/src/ex2_io.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ex2_io {
+
+import java.util.Scanner;
+
+public class ex2_io {
+    public static void main(String[] args) {
+        System.out.println("Taking inputs form the user");
+        Scanner sc = new Scanner(System.in); // System.in reads form the keyboard
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.print("The sum of these two number is: ");
+        System.out.println(sum);
+    }
 }
